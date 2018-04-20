a027aa2b84131b1acbd06d061e4798defc9de3d2
juan871003 <juan871003@gmail.com>
2018-04-20 17:01:19 +1000

    HTTP Calls module completed, next is Project Structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import './index.css';
 
-import {getUsers} from './api/userApi';
+import {getUsers, deleteUser} from './api/userApi';
 
 // Populate table of users via API call.
 getUsers().then(result => {
@@ -14,5 +14,20 @@ getUsers().then(result => {
       <td>${user.email}</td>
       </tr>`;
   });
+
   global.document.getElementById('users').innerHTML = usersBody;
+
+  const deleteLinks = glogal.document.getElementsByClassName('deleteUser');
+
+  // Must use array.from to create a real array from DOM collection
+  // getElementsByClassName only returns an 'array like' object
+  Array.from(deleteLinks, link => {
+    link.onClick = function(event) {
+      const element = event.target;
+      event.prevenDefault();
+      deleteUser(element.attributes['data-id'].value);
+      const row = element.parentNode.panentNode;
+      row.parentNode.removeChild(row);
+    };
+  });
 });
